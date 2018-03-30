3575193945f936b91ce26786b77f3f4709278c2f
gendium <acehugh@gmail.com>
2018-03-30 03:54:33 -0400

    updated tour

--- Project2/Tour.swift
+++ Project2/Tour.swift
@@ -12,14 +12,45 @@ class Tour : NSObject{
     var type : String
     var name: String
     var file : String
-    //var favorite : Bool
+    var favorite : Bool
     
     init(type: String, name: String, file: String)
     {
         self.type = type
         self.name = name
         self.file = file
-        //self.favorite = favorite
+        self.favorite = false
     }
-
+    
+    init(type: String, name: String, file: String, favorite : Bool)
+    {
+        self.type = type
+        self.name = name
+        self.file = file
+        self.favorite = favorite
+    }
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: propertyKey.name)
+        aCoder.encode(type, forKey: propertyKey.type)
+        aCoder.encode(file, forKey: propertyKey.file)
+        
+        
+        aCoder.encode(favorite, forKey: propertyKey.favorite)
+        
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObject(forKey: propertyKey.name) as? String
+        let type = aDecoder.decodeObject(forKey: propertyKey.type) as? String
+        let file = aDecoder.decodeObject(forKey: propertyKey.file) as? String
+        let favorite = aDecoder.decodeObject(forKey: propertyKey.favorite) as? Bool
+        self.init(type : type!, name: name!, file : file!, favorite : favorite!)
+    }
+    
+    struct propertyKey{
+        static let name = "name"
+        static let type = "type"
+        static let file = "file"
+        static let favorite = "favorite"
+    }
 }
