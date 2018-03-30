1e843a47a4c8eca3a3212909f02c4e0a6db6da33
gendium <acehugh@gmail.com>
2018-03-30 02:06:09 -0400

    dasdadasda

--- Project2/ViewControllers/NoteDetailViewController.swift
+++ Project2/ViewControllers/NoteDetailViewController.swift
@@ -5,11 +5,10 @@
 //  Created by Egger, Sean on 3/27/18.
 //  Copyright © 2018 Egger, Sean. All rights reserved.
 //
-
 import UIKit
 
 class NoteDetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+    
     
     var note: Note?
     
@@ -29,7 +28,7 @@ class NoteDetailViewController: UIViewController, UINavigationControllerDelegate
     
     /*
      Neccessary for implementing image picker. Sets the thumbnail and the note thumbnail to the chosen image
-    */
+     */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
     {
         //get the image
@@ -67,7 +66,7 @@ class NoteDetailViewController: UIViewController, UINavigationControllerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
     }
@@ -88,17 +87,16 @@ class NoteDetailViewController: UIViewController, UINavigationControllerDelegate
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
